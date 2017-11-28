82229f7fccc62eaad1737b6e187e5b45a98ff9b7
avasupertramp <efriedl.its-b2013@fh-salzburg.ac.at>
2017-11-28 17:30:50 +0100

    setup

---
+++ PCCP_L01/PCCP_L01/PCCP_L01.cpp
@@ -0,0 +1,12 @@
+// PCCP_L01.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+using namespace std;
+
+int main()
+{
+	cout << "hallo" << endl;
+	return 0;
+}
+
