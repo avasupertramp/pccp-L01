f748501016c4247e475fc390ffe21b1e23f0cf48
lualt <lukas@altenhuber.net>
2017-12-06 23:20:58 +0100

    added interpreting arguments

--- PCCP_L01/PCCP_L01/PCCP_L01.cpp
+++ PCCP_L01/PCCP_L01/PCCP_L01.cpp
@@ -2,11 +2,63 @@
 //
 
 #include "stdafx.h"
+#include <string>
 using namespace std;
 
-int main()
+
+void printHelp() {
+	cout << "help:" << endl;
+	cout << "TODO implement" << endl;
+}
+
+
+int main(int argc, char* argv[])
 {
-	cout << "hallo" << endl;
+	int depth = 0;
+	int maxThreads = 0;
+	string filter;
+	bool printProcesTime = false;
+	bool extendedOutput = false;
+	bool waitBeforeTerminate = false;
+
+
+	if (argc < 2) {
+		cout << "To less arguemnts" << endl;
+		return 0;
+	}
+
+	string path = argv[argc - 1];
+	for (int i = 1; i < (argc-1); i++) {
+		try {
+		string argument = argv[i];
+			if (argument == "-r") {
+				depth = stoi(argv[i + 1]);
+				i++;
+			}else if(argument == "-f") {
+				filter = argv[i + 1];
+				i++;
+			}else if (argument == "-t") {
+				maxThreads = stoi(argv[i + 1]);
+				i++;
+			}else if (argument == "-h") {
+				printHelp();
+				break;
+			}else if (argument == "-p") {
+				printProcesTime = true;
+			}else if (argument == "-v") {
+				extendedOutput = true;
+			}else if (argument == "-w") {
+				waitBeforeTerminate = true;
+			}
+			else {
+				cout << "false argument" << endl;
+			}
+		}
+		catch (exception e) {
+			fprintf(stderr, "Error in interpreting arguments %s",argv[i]);
+		}
+
+		}
 	return 0;
 }
 
